ac7ec4bba37d38bbfa8a39f38c31f808dde6cc3a
buffalo0707 <alan.henderson@libertymutual.com>
2017-05-23 14:00:09 -0400

    Adds trades resource
    Trades include 2 parties, each with a game.

---
+++ app/models/trade.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+  parties: [{
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    game: {
+      name: {
+        type: String,
+        required: true
+      },
+      system: {
+        type: String,
+        required: true,
+        enum: ['PS4', 'Xbox One', 'Switch', 'WiiU', 'Wii', 'Xbox 360', 'PS3', 'PS2', 'Xbox', 'Nindento DS', 'Nintendo 3DS']
+      }
+    }
+  }]
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Listing = mongoose.model('Listing', listingSchema)
+
+module.exports = Listing
