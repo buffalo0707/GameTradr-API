08b8e84a2e635a4007aac33e9e4c190dbc170c9c
buffalo0707 <alan.henderson@libertymutual.com>
2017-05-23 07:38:57 -0400

    Adds show and index methods for trades
    Does not require authentication.

--- app/controllers/trades.js
+++ app/controllers/trades.js
@@ -6,7 +6,22 @@ const Trade = models.trade
 
 const authenticate = require('./concerns/authenticate')
 // const setUser = require('./concerns/set-current-user')
-// const setModel = require('./concerns/set-mongoose-model')
+const setModel = require('./concerns/set-mongoose-model')
+
+const index = (req, res, next) => {
+  Trade.find()
+    .then(trades => res.json({
+      trades: trades.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
+}
+
+const show = (req, res) => {
+  res.json({
+    trade: req.trade.toJSON({ virtuals: true, user: req.user })
+  })
+}
 
 const create = (req, res, next) => {
   const trade = Object.assign(req.body.trade, {
@@ -22,10 +37,12 @@ const create = (req, res, next) => {
 }
 
 module.exports = controller({
+  index,
+  show,
   create
 }, { before: [
   // { method: setUser, only: ['index', 'show'] },
-  { method: authenticate, except: ['index', 'show'] }
-  // { method: setModel(Trade), only: ['show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Trade), only: ['show'] }
   // { method: setModel(Trade, { forUser: true }), only: ['update', 'destroy'] }
 ] })
